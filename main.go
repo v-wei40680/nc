d6e974ce6807284619d2a58db632d8fe9be0ad53
v-wei40680 <wei40680@pm.me>
2022-08-20 11:45:57 +0800

    add gin_mem bin

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	port := os.Getenv("PORT")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":" + port) // listen and serve on 0.0.0.0:8080
 }
-
